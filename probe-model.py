e2b7749dd8675c61987e3761329f83f3bfcea7f7
bstungnguyen <137641888+bstungnguyen@users.noreply.github.com>
2024-04-03 10:22:12 +0700

    Add files via upload

---
+++ probe-model.py
@@ -0,0 +1,23 @@
+#!/bin/env python
+
+"""Little command-line utility for probing a model on disk."""
+
+import argparse
+from pathlib import Path
+
+from invokeai.backend.model_manager import InvalidModelConfigException, ModelProbe
+
+parser = argparse.ArgumentParser(description="Probe model type")
+parser.add_argument(
+    "model_path",
+    type=Path,
+    nargs="+",
+)
+args = parser.parse_args()
+
+for path in args.model_path:
+    try:
+        info = ModelProbe.probe(path)
+        print(f"{path}:{info.model_dump_json(indent=4)}")
+    except InvalidModelConfigException as exc:
+        print(exc)
